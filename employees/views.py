c9cf3f9518f900234cf49e1726451ec82bd302c2
Maciej <sam.maciej@interia.pl>
2018-10-17 16:29:43 +0200

    Add views for Report model

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import viewsets
 
-# Create your views here.
+from employees.models import Report
+from employees.serializers import ReportSerializer
+
+
+class ReportViewSet(viewsets.ModelViewSet):
+    serializer_class = ReportSerializer
+    permission_classes = (
+        permissions.IsAuthenticated,
+        )
+
+    def get_queryset(self):
+        return Report.objects.filter(author=self.request.user).order_by('-date')
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
