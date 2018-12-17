fe303559ca2eb592df85d2d33560d76e4988f765
Maciej <sam.maciej@interia.pl>
2018-12-17 17:19:20 +0100

    Add pytest calls for other apps in run-test-coverage.sh

--- run-test-coverage.sh
+++ run-test-coverage.sh
@@ -1,10 +1,6 @@
-#Add here scripts for performing test coverage in following fashion, for each individual app in project:
-
-#cd <app catalogue name>
-#pytest 					\
-#	--cov-report term-missing 	\
-#	--cov-config ../coverage-config \
-#	--cov=.
-#rm .coverage
-#cd ..
-
+#!/usr/bin/env bash
+pytest 					        \
+	--cov-report term-missing 	\
+	--cov-config coverage-config \
+	--cov=.
+rm .coverage
