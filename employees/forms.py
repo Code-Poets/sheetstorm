2fd0dd7d4ef12cc88d55d00541ae622fa0154a41
Maciej <sam.maciej@interia.pl>
2018-12-21 16:52:44 +0100

    Add form for project joining feature
    Adds form with singular `ChoiceField` which is supposed to contain a
    list of projects the user can join to.

---
+++ employees/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.db.models import QuerySet
+
+
+class ProjectJoinForm(forms.Form):
+
+    projects = forms.ChoiceField(choices=[])
+
+    def __init__(self, queryset, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        assert isinstance(queryset, QuerySet)
+        self.fields['projects'].choices = [(project.id, project.name) for project in queryset]
