30362a92a5ff47dc3299cb084377c02f7b395ca6
Maciej <sam.maciej@interia.pl>
2019-01-09 14:06:04 +0100

    Add custom serializer field for hours input
    Adds custom field derived from `DecimalField` with overriden
    `to_internal_value` method. If field input contains *:* symbol, it will
    be converted to a *.* symbol before being processed through validation.

--- employees/serializers.py
+++ employees/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
 
 from employees.common.constants import ReportModelConstants
+from employees.common.validators import MaxDecimalValueValidator
 from employees.models import Report
 from managers.models import Project
+
+
+class HoursField(serializers.DecimalField):
+    def __init__(self, **kwargs):
+        super().__init__(
+            max_value=ReportModelConstants.MAX_WORK_HOURS.value,
+            min_value=ReportModelConstants.MIN_WORK_HOURS.value,
+            max_digits=ReportModelConstants.MAX_DIGITS.value,
+            decimal_places=ReportModelConstants.DECIMAL_PLACES.value,
+            validators=[MaxDecimalValueValidator(ReportModelConstants.MAX_WORK_HOURS_DECIMAL_VALUE.value)],
+            **kwargs)
+
+    def to_internal_value(self, data):
+        if isinstance(data, str) and ':' in data:
+            converted = data.replace(':', '.')
+            return super().to_internal_value(converted)
+        return super().to_internal_value(data)
 
 
 class ReportSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,10 +30,13 @@ class ReportSerializer(serializers.HyperlinkedModelSerializer):
         slug_field='name',
     )
     author = serializers.StringRelatedField()
+
     description = serializers.CharField(
         style={'base_template': 'textarea.html'},
         max_length=ReportModelConstants.MAX_DESCRIPTION_LENGTH.value
     )
+
+    work_hours = HoursField()
 
     class Meta:
         model = Report
