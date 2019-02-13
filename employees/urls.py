f809d0558f8441fbb07fbacd3601a859715f46b1
Maciej <sam.maciej@interia.pl>
2019-02-13 12:31:30 +0100

    Link admin's report detail view in url config

--- employees/urls.py
+++ employees/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     url(r"^reports/(?P<pk>[0-9]+)/$", views.ReportDetail.as_view(), name="custom-report-detail"),
     url(r"^reports/(?P<pk>[0-9]+)/delete/$", views.delete_report, name="custom-report-delete"),
     url(r"^reports/author/(?P<pk>[0-9]+)/$", views.AuthorReportView.as_view(), name="author-report-list"),
+    url(r"^reports/management/(?P<pk>[0-9]+)/$", views.AdminReportDetail.as_view(), name="admin-report-detail"),
 ]
