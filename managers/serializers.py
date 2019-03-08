8655d844e3747a7072a8296c9e980c0112d33e90
Martyna <martyna.gottschling@gmail.com>
2019-03-08 21:04:47 +0100

    Create ProjectCreate serializer

--- managers/serializers.py
+++ managers/serializers.py
@@ -7,3 +7,15 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = "__all__"
+
+
+class ProjectCreateSerializer(ProjectSerializer):
+    class Meta:
+        model = Project
+        fields = (
+            'name',
+            'start_date',
+            'terminated',
+            'managers',
+            'members',
+        )
