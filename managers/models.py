d7c4f780c61e5445f1dc73660071902e195e526f
Szymi1994 <mszymczak_94@wp.pl>
2019-03-11 11:18:45 +0100

    Add trigger to update user_type field depends on his role in projects

--- managers/models.py
+++ managers/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.db.models import Q
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 
 from managers.commons.constants import MAX_NAME_LENGTH
+from users.common.strings import CustomUserUserTypeText
 from users.models import CustomUser
 
 
@@ -28,3 +31,35 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+
+@receiver(m2m_changed, sender=Project.managers.through)
+def update_user_type(sender, action, pk_set, **kwargs):
+    project = kwargs['instance']
+    if action == 'pre_remove' or action == 'post_remove':
+        change_user_type_to_employee(pk_set)
+    elif action == 'pre_add' or action == "post_add":
+        change_user_type_to_manager(project)
+    else:
+        return
+
+
+def change_user_type_to_manager(project):
+    for manager in project.managers.all():
+        if manager.user_type != CustomUser.UserType.MANAGER.name:
+            manager.user_type = CustomUser.UserType.MANAGER.name
+            manager.full_clean()
+            manager.save()
+        else:
+            return
+
+
+def change_user_type_to_employee(pk_set):
+    for user_id in pk_set:
+        user = CustomUser.objects.get(pk=user_id)
+        if not user.manager_projects.exists():
+            user.user_type = CustomUser.UserType.EMPLOYEE.name
+            user.full_clean()
+            user.save()
+        else:
+            return
