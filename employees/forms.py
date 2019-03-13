25b6edb883e567dd2b81689108c4b280710c02f1
Maciej <sam.maciej@interia.pl>
2019-03-13 14:19:50 +0100

    Add form for switching months in report list view

--- employees/forms.py
+++ employees/forms.py
@@ -1,14 +1,22 @@
+import datetime
 from typing import Any
+from typing import Optional
 
 from bootstrap_datepicker_plus import DatePickerInput
 from django import forms
 from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
 from django.db.models import QuerySet
 from django.forms import TextInput
 from django.utils.dateparse import parse_duration
 
 from common.convert import convert_string_work_hours_field_to_hour_and_minutes
 from common.convert import timedelta_to_string
+from employees.common.constants import MONTH_NAVIGATION_FORM_MAX_MONTH_VALUE
+from employees.common.constants import MONTH_NAVIGATION_FORM_MAX_YEAR_VALUE
+from employees.common.constants import MONTH_NAVIGATION_FORM_MIN_MONTH_VALUE
+from employees.common.constants import MONTH_NAVIGATION_FORM_MIN_YEAR_VALUE
 from employees.common.strings import ReportValidationStrings
 from employees.models import Report
 
@@ -49,3 +57,43 @@ class ReportForm(forms.ModelForm):
     def __init__(self, *args: Any, **kwargs: Any):
         super(ReportForm, self).__init__(*args, **kwargs)
         self.fields["project"].queryset = kwargs["instance"].author.projects.all()
+
+
+class MonthSwitchForm(forms.Form):
+
+    date = forms.DateField(
+        widget=DatePickerInput(format="%m-%Y"),
+        label=False,
+        validators=[
+            MaxValueValidator(
+                datetime.date(
+                    year=MONTH_NAVIGATION_FORM_MAX_YEAR_VALUE, month=MONTH_NAVIGATION_FORM_MAX_MONTH_VALUE, day=1
+                )
+            ),
+            MinValueValidator(
+                datetime.date(
+                    year=MONTH_NAVIGATION_FORM_MIN_YEAR_VALUE, month=MONTH_NAVIGATION_FORM_MIN_MONTH_VALUE, day=1
+                )
+            ),
+        ],
+    )
+
+    def __init__(  # pylint: disable=keyword-arg-before-vararg
+        self, initial_date: Optional[datetime.date] = None, *args: Any, **kwargs: Any
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        if initial_date is not None:
+            self.fields["date"].initial = initial_date
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, MonthSwitchForm):
+            return NotImplemented
+        self_dict = self.fields["date"].__dict__
+        other_dict = other.fields["date"].__dict__
+        for key in self_dict.keys():
+            if key == "widget":
+                if self_dict[key].__dict__ != other_dict[key].__dict__:
+                    return False
+            elif self_dict[key] != other_dict[key]:
+                return False
+        return True
