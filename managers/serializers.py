475c73e9828ee284eec1291a9001884a96654d28
Radoslaw Wrzesien <rwrzesien@gmail.com>
2019-04-08 00:45:07 +0200

    Remove ProjectCreateSerializer

--- managers/serializers.py
+++ managers/serializers.py
@@ -7,15 +7,3 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = "__all__"
-
-
-class ProjectCreateSerializer(ProjectSerializer):
-    class Meta:
-        model = Project
-        fields = (
-            'name',
-            'start_date',
-            'terminated',
-            'managers',
-            'members',
-        )
