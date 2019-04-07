e926bc369402ebad52a6db8842d82d84ab49660c
Radoslaw Wrzesien <rwrzesien@gmail.com>
2019-04-08 00:44:16 +0200

    Change ProjectCreateView to Django generic view and generalize templates for ProjectCreateView and ProjectUpdateView

--- managers/urls.py
+++ managers/urls.py
@@ -10,7 +10,7 @@ router.register(r"projects", views.ProjectViewSet, "project")
 urlpatterns = [
     url("^api/", include(router.urls)),
     url("^projects/$", views.ProjectsList.as_view(), name="custom-projects-list"),
-    url("^projects/create/$", views.ProjectCreate.as_view(), name="custom-project-create"),
+    url("^projects/create/$", views.ProjectCreateView.as_view(), name="custom-project-create"),
     url("^projects/(?P<pk>[0-9]+)/$", views.ProjectDetail.as_view(), name="custom-project-detail"),
     url("^projects/(?P<pk>[0-9]+)/update/$", views.ProjectUpdateView.as_view(), name="custom-project-update"),
 ]
