f4e67ba368dfed6fd1adfd3dd0031e6a653db2b5
Karol Beker <karolbeker@gmail.com>
2019-04-12 16:13:18 +0200

    Add raven to base settings

--- sheetstorm/settings/base.py
+++ sheetstorm/settings/base.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'managers.apps.ManagersConfig',
     'employees.apps.EmployeesConfig',
+    'raven.contrib.django.raven_compat',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'raven.contrib.django.raven_compat.middleware.Sentry404CatchMiddleware',
 ]
 
 ROOT_URLCONF = 'sheetstorm.urls'
