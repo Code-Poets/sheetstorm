3fb2c3bf3392aa626d57d3f584ba909340c0fc96
Radoslaw Wrzesien <rwrzesien@gmail.com>
2019-04-15 02:31:57 +0200

    Set django email backends for develop and production

--- sheetstorm/settings/base.py
+++ sheetstorm/settings/base.py
@@ -259,9 +259,7 @@ REST_AUTH_REGISTER_SERIALIZERS = {
 }
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 SITE_ID = 1
 
