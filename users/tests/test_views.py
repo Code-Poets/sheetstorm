ea23b5df4d25c95eb2a5f02d976626f84c526976
Radoslaw Wrzesien <rwrzesien@gmail.com>
2019-04-15 02:33:11 +0200

    Update password change tests

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -2,18 +2,18 @@ from django.test import TestCase
 from rest_framework.reverse import reverse
 
 from users.common.strings import CustomValidationErrorText
-from users.common.utils import create_user_using_full_clean_and_save
+from users.factories import UserFactory
 
 
 class ChangePasswordTests(TestCase):
     def setUp(self):
         self.user_password = "userpasswd"
-        self.user = create_user_using_full_clean_and_save("testuser@codepoets.it", "", "", "", self.user_password)
+        self.user = UserFactory()
 
     def test_change_user_password_view_should_change_user_password_on_post(self):
         data = {"old_password": self.user_password, "new_password1": "newuserpasswd", "new_password2": "newuserpasswd"}
         self.client.login(email=self.user.email, password=self.user_password)
-        response = self.client.post(reverse("change_password"), data)
+        response = self.client.post(reverse("password_change"), data)
         self.user.refresh_from_db()
         self.assertEqual(response.status_code, 302)
         self.assertTrue(self.user.check_password(data["new_password1"]))
@@ -21,13 +21,9 @@ class ChangePasswordTests(TestCase):
     def test_change_user_password_view_should_not_change_user_password_when_old_password_is_incorrect(self):
         data = {"old_password": "wronguserpasswd", "new_password1": "newuserpasswd", "new_password2": "newuserpasswd"}
         self.client.login(email=self.user.email, password=self.user_password)
-        response = self.client.post(reverse("change_password"), data)
+        response = self.client.post(reverse("password_change"), data)
         self.user.refresh_from_db()
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(
-            CustomValidationErrorText.VALIDATION_ERROR_CHANGE_PASSWORD_MESSAGE
-            in response.context["form"].errors.get("old_password")
-        )
         self.assertFalse(self.user.check_password(data["new_password1"]))
         self.assertFalse(self.user.check_password(data["old_password"]))
         self.assertTrue(self.user.check_password(self.user_password))
@@ -39,7 +35,7 @@ class ChangePasswordTests(TestCase):
             "new_password2": "notthesamenewuserpasswd",
         }
         self.client.login(email=self.user.email, password=self.user_password)
-        response = self.client.post(reverse("change_password"), data)
+        response = self.client.post(reverse("password_change"), data)
         self.user.refresh_from_db()
         self.assertEqual(response.status_code, 200)
         self.assertTrue(
