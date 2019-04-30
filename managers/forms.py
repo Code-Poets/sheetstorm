998b547878ccc1d5be5a9ac93e0824bf3ce267c0
Szymi1994 <mszymczak_94@wp.pl>
2019-04-30 15:43:22 +0200

    [M] Add help text to date picker input

--- managers/forms.py
+++ managers/forms.py
@@ -1,6 +1,7 @@
 from bootstrap_datepicker_plus import DatePickerInput
 from django import forms
 
+from managers.commons.constants import CORRECT_DATE_FORMAT
 from managers.models import Project
 
 
@@ -8,4 +9,7 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = "__all__"
-        widgets = {"start_date": DatePickerInput(format="%Y-%m-%d"), "stop_date": DatePickerInput(format="%Y-%m-%d")}
+        widgets = {
+            "start_date": DatePickerInput(options={"format": CORRECT_DATE_FORMAT}),
+            "stop_date": DatePickerInput(options={"format": CORRECT_DATE_FORMAT}),
+        }
