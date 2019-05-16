eb10fec61b8877c0b0352ff9c783a8ed6823ad7c
Radoslaw Wrzesien <rwrzesien@gmail.com>
2019-05-16 03:05:36 +0200

    Change project detail to Django view

--- managers/urls.py
+++ managers/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url("^api/", include(router.urls)),
     url("^projects/$", views.ProjectsList.as_view(), name="custom-projects-list"),
     url("^projects/create/$", views.ProjectCreateView.as_view(), name="custom-project-create"),
-    url("^projects/(?P<pk>[0-9]+)/$", views.ProjectDetail.as_view(), name="custom-project-detail"),
+    url("^projects/(?P<pk>[0-9]+)/$", views.ProjectDetailView.as_view(), name="custom-project-detail"),
     url("^projects/(?P<pk>[0-9]+)/update/$", views.ProjectUpdateView.as_view(), name="custom-project-update"),
     url("^projects/(?P<pk>[0-9]+)/delete/$", views.ProjectDeleteView.as_view(), name="custom-project-delete"),
 ]
