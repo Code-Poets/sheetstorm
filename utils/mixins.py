2dd873e8bf53a5ddac639c4038602e4fa2fe890a
Radoslaw Wrzesien <rwrzesien@gmail.com>
2019-05-24 01:15:24 +0200

    Add access permissions view mixin classes and tests for them

--- utils/mixins.py
+++ utils/mixins.py
@@ -1,2 +1,29 @@
+from django.db.models import QuerySet
+
+
 class NotCallableMixin:
     do_not_call_in_templates = True
+
+
+class UserIsManagerOfCurrentProjectMixin:
+    def get_queryset(self) -> QuerySet:
+        if self.request.user.is_manager:  # type: ignore
+            return super().get_queryset().filter(managers=self.request.user.pk)  # type: ignore
+        else:
+            return super().get_queryset()  # type: ignore
+
+
+class UserIsManagerOfCurrentReportProjectMixin:
+    def get_queryset(self) -> QuerySet:
+        if self.request.user.is_manager:  # type: ignore
+            return super().get_queryset().filter(project__managers=self.request.user.pk)  # type: ignore
+        else:
+            return super().get_queryset()  # type: ignore
+
+
+class UserIsAuthorOfCurrentReportMixin:
+    def get_queryset(self) -> QuerySet:
+        if self.request.user.is_employee:  # type: ignore
+            return super().get_queryset().filter(author=self.request.user.pk)  # type: ignore
+        else:
+            return super().get_queryset()  # type: ignore
---
+++ utils/tests/test_mixins.py
@@ -0,0 +1,149 @@
+from django.test import RequestFactory
+from django.test import TestCase
+from django.views.generic import ListView
+
+from employees.factories import ReportFactory
+from employees.models import Report
+from managers.factories import ProjectFactory
+from managers.models import Project
+from users.factories import UserFactory
+from users.models import CustomUser
+from utils.mixins import UserIsAuthorOfCurrentReportMixin
+from utils.mixins import UserIsManagerOfCurrentProjectMixin
+from utils.mixins import UserIsManagerOfCurrentReportProjectMixin
+
+
+class UserIsAuthorOfCurrentReportMixinTestCase(TestCase):
+    def setUp(self):
+        super().setUp()
+
+        class TestView(UserIsAuthorOfCurrentReportMixin, ListView):
+            model = Report
+
+        self.view = TestView.as_view()
+        self.request_factory = RequestFactory()
+
+    def test_that_user_is_author_of_current_report_mixin_should_limit_view_report_queryset_if_user_is_employee(self):
+        user = UserFactory(user_type=CustomUser.UserType.EMPLOYEE.name)
+
+        author_report = ReportFactory(author=user)
+        # Other author report.
+        ReportFactory()
+
+        assert Report.objects.count() == 2
+
+        request = self.request_factory.get("anything")
+        request.user = user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context_data["object_list"]), 1)
+        self.assertEqual(response.context_data["object_list"][0].pk, author_report.pk)
+
+    def test_that_user_is_author_of_current_report_mixin_should_not_limit_view_report_queryset_if_user_is_not_employee(
+        self
+    ):
+        user = UserFactory(user_type=CustomUser.UserType.MANAGER.name)
+
+        ReportFactory(author=user)
+        # Other author report.
+        ReportFactory()
+
+        assert Report.objects.count() == 2
+
+        request = self.request_factory.get("anything")
+        request.user = user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context_data["object_list"]), 2)
+
+
+class UserIsManagerOfCurrentProjectMixinTestCase(TestCase):
+    def setUp(self):
+        super().setUp()
+
+        class TestView(UserIsManagerOfCurrentProjectMixin, ListView):
+            model = Project
+
+        self.view = TestView.as_view()
+        self.request_factory = RequestFactory()
+
+    def test_that_user_is_manager_of_current_project_mixin_should_limit_view_project_queryset_if_user_is_manager(self):
+        user = UserFactory(user_type=CustomUser.UserType.MANAGER.name)
+
+        manager_project = ProjectFactory()
+        manager_project.managers.add(user)
+        # Project without current user as manager.
+        ProjectFactory()
+
+        assert Project.objects.count() == 2
+
+        request = self.request_factory.get("anything")
+        request.user = user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context_data["object_list"]), 1)
+        self.assertEqual(response.context_data["object_list"][0].pk, manager_project.pk)
+
+    def test_that_user_is_manager_of_current_project_mixin_should_not_limit_view_project_queryset_if_user_is_not_manager(
+        self
+    ):
+        user = UserFactory(user_type=CustomUser.UserType.EMPLOYEE.name)
+
+        manager_project = ProjectFactory()
+        manager_project.managers.add(user)
+        # Project without current user as manager.
+        ProjectFactory()
+
+        assert Project.objects.count() == 2
+
+        request = self.request_factory.get("anything")
+        request.user = user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context_data["object_list"]), 2)
+
+
+class UserIsManagerOfCurrentReportProjectMixinTestCase(TestCase):
+    def setUp(self):
+        super().setUp()
+
+        class TestView(UserIsManagerOfCurrentReportProjectMixin, ListView):
+            model = Report
+
+        self.view = TestView.as_view()
+        self.request_factory = RequestFactory()
+
+    def test_that_user_is_manager_of_current_project_mixin_should_limit_view_project_queryset_if_user_is_manager(self):
+        user = UserFactory(user_type=CustomUser.UserType.MANAGER.name)
+
+        report = ReportFactory()
+        report.project.managers.add(user)
+        # Report with project without current user as manager.
+        ReportFactory()
+
+        assert Report.objects.count() == 2
+
+        request = self.request_factory.get("anything")
+        request.user = user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context_data["object_list"]), 1)
+        self.assertEqual(response.context_data["object_list"][0].pk, report.pk)
+
+    def test_that_user_is_manager_of_current_project_mixin_should_not_limit_view_project_queryset_if_user_is_not_manager(
+        self
+    ):
+        user = UserFactory(user_type=CustomUser.UserType.EMPLOYEE.name)
+
+        report = ReportFactory()
+        report.project.managers.add(user)
+        # Report with project without current user as manager.
+        ReportFactory()
+
+        assert Report.objects.count() == 2
+
+        request = self.request_factory.get("anything")
+        request.user = user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context_data["object_list"]), 2)
