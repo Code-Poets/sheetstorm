bfea5bfa5726301ed696b407e31e0cec465a2869
Karol Beker <karolbeker@gmail.com>
2019-07-04 16:29:29 +0200

    Change manager app constants to enums

--- managers/commons/constants.py
+++ managers/commons/constants.py
@@ -1,7 +1,11 @@
+from enum import Enum
+
 from django.utils.translation import ugettext_lazy as _
 
 from common.constants import CORRECT_DATE_FORMAT
 
-MAX_NAME_LENGTH = 64
-MESSAGE_FOR_CORRECT_DATE_FORMAT = f"Please enter date in this format: {CORRECT_DATE_FORMAT}"
-STOP_DATE_VALIDATION_ERROR_MESSAGE = _("A project can not be created after expired date!")
+
+class ProjectConstants(Enum):
+    MAX_NAME_LENGTH = 64
+    MESSAGE_FOR_CORRECT_DATE_FORMAT = f"Please enter date in this format: {CORRECT_DATE_FORMAT}"
+    STOP_DATE_VALIDATION_ERROR_MESSAGE = _("A project can not be created after expired date!")
