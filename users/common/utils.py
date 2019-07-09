1308177fa8455ea670f30ba67df573077ee83971
Karol Beker <karolbeker@gmail.com>
2019-07-09 19:15:20 +0200

    Add notifications feature

--- users/common/utils.py
+++ users/common/utils.py
@@ -1,6 +1,8 @@
+import datetime
 import logging
 import random
 import string
+from typing import Optional
 
 logger = logging.getLogger(__name__)
 
@@ -11,3 +13,13 @@ def generate_random_string_from_letters_and_digits(length):
 
 def generate_random_phone_number(length):
     return "".join(random.choices(string.digits, k=length))
+
+
+def count_workdays(start_date: datetime.date, days_number: int) -> Optional[int]:
+    return sum(
+        [
+            True
+            for days_delta in range(1, days_number)
+            if (start_date + datetime.timedelta(days=days_delta)).isoweekday() < 6
+        ]
+    )
