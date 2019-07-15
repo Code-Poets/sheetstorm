fd23e7b48f87ad9ac838923b1f4e2ab93c140500
Maciej <sam.maciej@interia.pl>
2019-07-15 13:55:26 +0200

    Update full-check configuration

--- features/environment.py
+++ features/environment.py
@@ -1,3 +1,4 @@
+# pylint: disable=W0601, W0613
 from behave import use_fixture
 
 from features.fixtures import selenium_browser
--- lint.sh
+++ lint.sh
@@ -6,7 +6,7 @@ flake8                                                                          
     --jobs=4                                                                                                \
     --max-line-length=120                                                                                   \
     --ignore=E124,E126,E128,E131,E156,E201,E221,E222,E241,E265,E271,E272,E701,F405,E501,W503                \
-    --per-file-ignores=users/common/strings.py:E501
+    --per-file-ignores=features/steps/user_initialisation_steps.py:F403,F811
 printf "\n"
 
 printf "[PYLINT: sheetstorm]\n"
