6dce69817b3fb27db0101b1a6a80141ab0434deb
Karol Beker <karolbeker@gmail.com>
2019-07-31 09:58:14 +0200

    Improvement to check-environment-packages script

--- check-environment-packages.sh
+++ check-environment-packages.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
-
-pipenv --rm
-
-yes | pipenv install --dev --clear --three
-
-pipenv check
+#!/bin/bash -e
 
 if [[ -z "$VIRTUAL_ENV" ]]
 then
-      pipenv shell
+    RED_COLOR='\033[0;31m'
+    GREEN_COLOR='\033[0;32m'
+    NO_COLOR='\033[0m'
+    printf "${RED_COLOR}Script must be run with activated virtualenv!\n${NO_COLOR}You can activate shell by: ${GREEN_COLOR}pipenv shell\n"
+    exit 1
 fi
+
+pipenv uninstall --all
+
+yes | pipenv install --dev --clear
+
+pipenv check
