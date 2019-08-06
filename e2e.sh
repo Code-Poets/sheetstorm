a7f73b3ce1a2b506fbe6334a48dc8f04268d8e5b
Piotr Dybowski <piotr.dybowski@codepoets.it>
2019-08-06 14:47:32 +0200

    Modify `e2e.sh` script to include coverage of selenium tests

--- e2e.sh
+++ e2e.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 
-python manage.py behave --settings=sheetstorm.settings.testing --no-capture
+coverage run --source='.' --rcfile=coverage-config manage.py behave --settings=sheetstorm.settings.testing --no-capture
+coverage report
+rm .coverage
