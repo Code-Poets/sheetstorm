fd6e4da26395793c6dba1537de8bab706bf54820
Maciej <sam.maciej@interia.pl>
2020-05-31 21:18:41 +0200

    Refactor backend for work hours summary functionality

--- utils/mixins.py
+++ utils/mixins.py
@@ -1,8 +1,11 @@
-from datetime import date
 from typing import Any
+from typing import Dict
+from typing import Tuple
+from typing import Union
 
 from django.db.models import Q
 from django.db.models import QuerySet
+from django.db.models import Sum
 from django.utils import timezone
 from django.views.generic.base import ContextMixin
 
@@ -46,29 +49,42 @@ class ProjectsWorkPercentageMixin(ContextMixin):
 
         context_data = super().get_context_data(**kwargs)
 
-        from_date = None
-        to_date = None
+        report_set = self.object.report_set.all() if self.model is CustomUser else self.get_queryset()
 
-        if "year" in self.kwargs and "month" in self.kwargs:
-            year = int(self.kwargs["year"])
-            month = int(self.kwargs["month"])
-            current_date = timezone.now().date()
-            from_date = timezone.datetime(year=year, month=month, day=1).date()
-            if current_date.year == year and current_date.month == month:
-                to_date = current_date
-            else:
-                to_date = self._set_last_day_of_month_on_date(year=year, month=month)
-
-        if self.model == CustomUser:
-            user = self.object
-        else:
-            user = self.request.user
-
-        context_data["projects_work_percentage"] = user.get_projects_work_hours_and_percentage(from_date, to_date)
+        context_data["projects_work_percentage"] = self._get_projects_work_hours_and_percentage(report_set)
         return context_data
 
+    def _get_projects_work_hours_and_percentage(self, report_set: QuerySet) -> Dict[str, Any]:
+        """
+        Returns dict where keys are Project names and values are lists containing total sum of work hours
+        and percentage amount of work by this user.
+        """
+        work_hours_per_project = self._generate_hours_per_project_queryset_from_reports_queryset(report_set)
+        work_hours_sum = self._get_sum_total_of_work_hours_sum_from_hours_per_project_queryset(work_hours_per_project)
+        return self._get_dict_of_total_work_hours_per_project_statistics(work_hours_per_project, work_hours_sum)
+
     @staticmethod
-    def _set_last_day_of_month_on_date(year: int, month: int) -> date:
-        if month == 12:
-            return date(year=year, month=month, day=31)
-        return date(year=year, month=month + 1, day=1) - timezone.timedelta(days=1)
+    def _generate_hours_per_project_queryset_from_reports_queryset(reports_queryset: QuerySet) -> QuerySet:
+        return reports_queryset.values("project__name").annotate(project_hours=Sum("work_hours"))
+
+    @staticmethod
+    def _get_sum_total_of_work_hours_sum_from_hours_per_project_queryset(
+        hours_per_project_queryset: QuerySet
+    ) -> timezone.timedelta:
+        return hours_per_project_queryset.aggregate(Sum("project_hours"))["project_hours__sum"]
+
+    def _get_dict_of_total_work_hours_per_project_statistics(
+        self, queryset: QuerySet, work_hours_sum: timezone.timedelta
+    ) -> Dict[str, Any]:
+        return {
+            annotation["project__name"]: (
+                self._get_project_hours_sum_and_percentage(annotation["project_hours"], work_hours_sum)
+            )
+            for annotation in queryset
+        }
+
+    @staticmethod
+    def _get_project_hours_sum_and_percentage(
+        project_hours: timezone.timedelta, all_hours: timezone.timedelta
+    ) -> Union[Tuple[timezone.timedelta, float], int]:
+        return (project_hours, (project_hours / all_hours) * 100) if all_hours.total_seconds() > 0 else 0
